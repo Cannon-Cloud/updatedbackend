5c4dd530b7e3f529987f6b76bd6d82073dce2f71
Cannon-Cloud <clarence@cannon.cloud>
2021-08-08 16:32:15 -0700

    Orders Logic

--- models/order.js
+++ models/order.js
@@ -1,11 +1,51 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
+  orderItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'OrderItem',
+    },
+  ],
+  shippingAddress1: {
+    type: String,
     required: true,
+  },
+  shippingAddress2: {
+    type: String,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: 'Pending',
+  },
+  totalPrice: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  dateOrdered: {
+    type: Date,
+    default: Date.now,
   },
 });
 
@@ -18,4 +58,3 @@ orderSchema.set('toJSON', {
 });
 
 exports.Order = mongoose.model('Order', orderSchema);
-exports.orderSchema = orderSchema;
